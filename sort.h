3b009fb1dbfc7d072ec19aac179a0377045497b2
ouidane <ouidanezakaria@gmail.com>
2024-10-30 02:30:39 +0100

    update sort.h

--- sort.h
+++ sort.h
@@ -9,7 +9,7 @@
 #define DOWN 1
 
 /**
- * enum bool - Enumeration of Boolean values.
+ * enum bool - Enum of Boolean values.
  * @false: Equals 0.
  * @true: Equals 1.
  */
@@ -33,11 +33,8 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
