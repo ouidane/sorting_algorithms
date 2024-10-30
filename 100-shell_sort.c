582856706a3b442babbf9edfb06ddf8034bf0f4c
ouidane <ouidanezakaria@gmail.com>
2024-10-30 01:25:43 +0100

    task 4

---
+++ 100-shell_sort.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+
+/**
+ * _swap - Swap two integers in an array.
+ * @i: The first integer to swap.
+ * @j: The second integer to swap.
+ *
+ * Return: Nothing.
+ */
+void _swap(int *i, int *j)
+{
+	int tmp;
+
+	tmp = *i;
+	*i = *j;
+	*j = tmp;
+}
+
+/**
+ * shell_sort - Sort an array of integers in ascending
+ *              order using the shell sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Return: Nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				_swap(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
+}
