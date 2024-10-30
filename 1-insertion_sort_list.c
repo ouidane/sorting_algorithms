73419cba6aac2edb746a0a47c8d6224c18cff50d
ouidane <ouidanezakaria@gmail.com>
2024-10-30 01:06:50 +0100

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,6 +5,8 @@
  * @head: A pointer to the head of the doubly-linked list.
  * @node1: A pointer to the first node to swap.
  * @node2: The second node to swap.
+ *
+ * Return: Nothing.
  */
 void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2)
 {
@@ -22,11 +24,11 @@ void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2)
 }
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- *                       using the insertion sort algorithm.
+ * insertion_sort_list - sorts an array of integers in ascending order
+ *						using the Selection sort algorithm
  * @list: A pointer to the head of a doubly-linked list of integers.
  *
- * Description: Prints the list after each swap.
+ * Return: Nothing.
  */
 void insertion_sort_list(listint_t **list)
 {
