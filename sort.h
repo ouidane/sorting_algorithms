8cdb67f1d6cb3717c006ccfee59ad6923aa7f1ed
Aymen Hommani <141479352+Crosspii@users.noreply.github.com>
2024-10-30 03:33:24 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -32,6 +32,7 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+/*------------------------- FUNCTIONS ----------------------------*/
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
